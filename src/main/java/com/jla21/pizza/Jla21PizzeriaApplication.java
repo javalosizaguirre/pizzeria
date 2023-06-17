22d3040be24a9aba1759454471c3ae47422962c5
Jose Luis Avalos Izaguirre <j.avalosizaguirre@gmail.com>
2023-06-17 09:33:11 -0500

    Inicio

---
+++ src/main/java/com/jla21/pizza/Jla21PizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.jla21.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jla21PizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jla21PizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jla21/pizza/Jla21PizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jla21.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jla21PizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
